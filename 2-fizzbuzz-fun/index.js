60870b0a989a7b2632cac38c5abc4ade66a21e52
RJalbers <RJalbers89@gmail.com>
2020-01-22 20:54:17 -0800

    completed

--- 2-fizzbuzz-fun/index.js
+++ 2-fizzbuzz-fun/index.js
@@ -1,8 +1,8 @@
 /*
-Description:
-Input:
-Output:
-Usage: 
+Description:an adaptation of the FizzBuzz algorithm but with a function.
+Input:An integer
+Output: A list of numbers, with fizz is divisible by 3, buzz iff divisble by 5, and fizzbuzz if divisible by both
+Usage: usage node 2-fizzbuzz-fun <integer>
 */
 
 // get input using getargs
@@ -10,17 +10,27 @@ const getargs = require("../modules/getargs")
 let input = getargs.getIntegerArg()
 
 
-function fizzbuzz(n){
+function fizzbuzz(i){
 // TODO provide for tests for divisible for 3,5, and 15 to return desired output
+if (i % 15 == 0 ) {
+  return 'fizzbuzz';
+} else if (i % 3 == 0 ) {
+  return 'fizz';
+} else if (i % 5 == 0 ) {
+  console.log(`${i} buzz`);
+} else {
+  return '';
+}
+
 }
 
 // TODO check if input is not an integer
-if ( ) {
-  // TODO provide usage statement
+if (isNaN(input) || !Number.isInteger(input)) {
+  console.log("usage node 2-fizzbuzz-fun <integer>")
 }
 else {
   // TODO provide info to for loop to iterate from 1 to value of input
-  for ( ) {
+  for ( let i = 1; i <= input; i++ ) {
     console.log(`${i} ${fizzbuzz(i)}`);
   }
 }
--- 4-reverse-string-iterate/index.js
+++ 4-reverse-string-iterate/index.js
@@ -1,9 +1,9 @@
 //TODO fill in comment template
 /*
-Description:
-Input:
-Output:
-Usage: 
+Description: a program that takes a string as input and reverses it by iterating through the string, character by character.
+Input: A string
+Output: Reverse of a string
+Usage: node 4-reverse-string-iterate <string>
 */
 
 // get input using getargs
@@ -13,28 +13,39 @@ let input = getargs.getStringArg()
 function reverseWithFullIteration(str) {
     let result = '';
     // TODO loop from end of str (str.length-1) to beginning
-    for ( ) {
-        // TODO add the letter in the current position to the result string.
+    for ( let i = str.length - 1; i>=0 ; i--) {
+        
+        result += str[i];
     }
     return result;
 }
 
 function reverseWithHalfIteration(str) {
     // STRETCH TODO write a function that reverses the string but only iterates over half of the input string.
+    let result = ''
+    let reverseResult;
+    
+    for ( let i = 0; i < str.length / 2 ; i++) {
+
+        result += str[i]
+        reverseResult = result.split('').reverse().join('')
+  
+    }
+    return reverseResult
 }
 
 //test that it is a string
 //TODO check for string argument and if no string is entered provide a usage statement and quit
-if ( ) {
-    // TODO provide a usage statement
+if (!input) {
+    console.log('node 4-reverse-string-iterate <string>'); 
 }
 else {
     // use functions to reverse string
     console.log("result with full iteration", reverseWithFullIteration(input));
 
     // STRETCH TODO output the result of your reverseWithHalfIteration function
-
-}
+   
+}  console.log(reverseWithHalfIteration(input));
 
 
 
